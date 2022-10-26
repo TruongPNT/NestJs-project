0a4bb2e02a4347b8ee52f92f35e1ead6d5e4d0b3
Daniel N <daniel.phucthien@setel.com>
2022-10-26 16:56:56 +0700

    26/10

--- src/filter/http-exception.filter.ts
+++ src/filter/http-exception.filter.ts
@@ -13,14 +13,11 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
-    const message = exception.message;
 
-    response.status(status).json({
+    response.json({
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
-      description: 'Http Error',
-      message: message,
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
